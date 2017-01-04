da6eb0630c4cba52d6e51413e200a73fed66b888
Alan Heanue <g00318763@gmit.ie>
2017-01-04 23:06:59 +0000

    Added Server runner class

---
+++ src/ie/gmit/sw/server/ServerRunner.java
@@ -0,0 +1,24 @@
+
+package ie.gmit.sw.server;
+
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerRunner 
+{
+	
+	public static void main(String[] args) throws Exception 
+	
+	{
+	@SuppressWarnings("resource")
+		
+	ServerSocket m_ServerSocket = new ServerSocket(7777, 100 , InetAddress.getByName ("127.0.0.1") );
+		int id = 0;
+		
+		while (true) 
+		{
+		
+		}//end while 
+	}//main
+}
